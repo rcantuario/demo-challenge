721c998a7f9fdaa0d0659ed5ef2bfc1e6bb2fe11
rcantuario <r.cantuario@gmail.com>
2023-04-22 12:45:20 -0300

    Add data insertion during app startup

--- src/main/java/com/example/demo/service/DataStartup.java
+++ src/main/java/com/example/demo/service/DataStartup.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.model.BankAccount;
+import com.example.demo.model.Wallet;
 import com.example.demo.repository.BankAccountRepository;
+import com.example.demo.repository.WalletRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +15,14 @@ public class DataStartup {
     private final PaymentService paymentService;
     private final TransactionService transactionService;
     private final BankAccountRepository bankAccountRepository;
+    private final WalletRepository walletRepository;
 
     @PostConstruct
     public void init(){
-        bankAccountRepository.save(new BankAccount(10,"Vinicius","JR","459036","141259877","4569871"));
-        bankAccountRepository.save(new BankAccount(1,"Karim","Benzema","98375987","1234567","986578"));
+        bankAccountRepository.save(new BankAccount(1,"Tony","Stark","211927207","141259877","1885226711"));
+        //bankAccountRepository.save(new BankAccount(1,"Karim","Benzema","98375987","1234567","986578"));
+
+        walletRepository.save(new Wallet(1,"0245253419","028444018","USD"));
     }
 
 
